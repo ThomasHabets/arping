6021007fdbb008bc270bed15395b3d97752d8ccc
Thomas Habets <thomas@habets.se>
2012-10-03 18:39:39 +0100

    Unit test: check printed output

--- src/arping_test.c
+++ src/arping_test.c
@@ -1,9 +1,124 @@
 #include<pcap.h>
 #include<inttypes.h>
 #include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+
 #include<check.h>
+
 #include"arping.h"
 
+/**
+ *
+ */
+static void
+xclose(int* fd)
+{
+        if (0 > close(*fd)) {
+                fprintf(stderr, "close(%d): %s", *fd, strerror(errno));
+                *fd = -1;
+        }
+}
+
+struct captured_output {
+        int saved_fd;  // Old fd, will be dup2()ed back in place when done.
+        int fno;       // Overridden fd (e.g. stdout or stderr).
+        int reader_fd; // Reader end of the pipe.
+        char* buffer;  // Output buffer.
+        pthread_t thread;  // Reader thread.
+};
+
+/**
+ * Helper function for stdout/stderr catching.
+ *
+ * This is the main() for the thread that reads from the fake stdout pipe
+ * and writes into the buffer.
+ *
+ * FIXME: Has no buffer size checking.
+ */
+static void*
+read_main(void* p)
+{
+        struct captured_output* out = p;
+        char *cur = out->buffer;
+
+        while (1) {
+                ssize_t n;
+                n = read(out->reader_fd, cur, 1024);
+                if (n > 0) {
+                        cur += n;
+                }
+                if (n == 0) {
+                        break;
+                }
+        }
+}
+
+/**
+ * Helper function to capture stdout/stderr output.
+ *
+ * Args:
+ *   fd:  The fd to capture.
+ * Returns:
+ *   A structure to be used as a handle. Only thing caller should do with
+ *   this structure is call stop_capture(), read its .buffer member, and
+ *   uncapture().
+ */
+static struct captured_output*
+capture(int fd)
+{
+        struct captured_output* out;
+
+        out = calloc(1, sizeof(struct captured_output));
+        fail_if(out == NULL);
+
+        out->fno = fd;
+        out->saved_fd = dup(fd);
+        out->buffer = calloc(100, 1024);
+
+        fail_if(0 > out->saved_fd);
+        fail_if(out->buffer == NULL);
+
+        // set up pipe
+        int fds[2];
+        fail_if(0 > pipe(fds));
+        fail_if(0 > dup2(fds[1], fd));
+        out->reader_fd = fds[0];
+        xclose(&fds[1]);
+
+        fail_if(pthread_create(&out->thread, NULL, read_main, (void*)out));
+        return out;
+}
+
+/**
+ * Helper function to capture stdout/stderr output.
+ *
+ * Stop capture, so that .buffer becomes readable.
+ */
+static void
+stop_capture(struct captured_output* out)
+{
+        fail_if(0 > dup2(out->saved_fd, out->fno));
+        xclose(&out->saved_fd);
+        fail_if(pthread_join(out->thread, NULL));
+        xclose(&out->reader_fd);
+}
+
+/**
+ * Helper function to capture stdout/stderr output.
+ *
+ * Deallocate buffer. stop_capture() must be called before uncapture().
+ */
+static void
+uncapture(struct captured_output* out)
+{
+        free(out->buffer);
+        free(out);
+}
+
+/**
+ * Test that a bogus packet is ignored.
+ */
 START_TEST(uninteresting_packet)
 {
         struct pcap_pkthdr pkthdr;
@@ -11,11 +126,17 @@ START_TEST(uninteresting_packet)
 
         int prev_numrecvd = numrecvd;
 
+        struct captured_output *sout = capture(1);
         pingip_recv(NULL, &pkthdr, packet);
-
+        stop_capture(sout);
         fail_unless(prev_numrecvd == numrecvd);
+        fail_unless(strlen(sout->buffer) == 0);
+        uncapture(sout);
 } END_TEST
 
+/**
+ * Test that a matching packet is successfully handled.
+ */
 START_TEST(interesting_packet)
 {
         struct pcap_pkthdr pkthdr;
@@ -42,15 +163,41 @@ START_TEST(interesting_packet)
 
         dstip = htonl(0x12345678);
 
+        pkthdr.ts.tv_sec = time(NULL);
+        pkthdr.ts.tv_usec = 0;
+        pkthdr.len = 60;
+        pkthdr.caplen = 60;
+
+        struct captured_output *sout;
+
+        // First ping.
+        const char* correct0 =
+                "60 bytes from 77:88:99:aa:bb:cc (18.52.86.120): "
+                "index=0 time=";
+        sout = capture(1);
         pingip_recv(NULL, &pkthdr, packet);
+        stop_capture(sout);
         fail_unless(numrecvd == prev_numrecvd + 1,
                     "numrecvd not incremented");
+        fail_unless(!strncmp(sout->buffer, correct0, strlen(correct0)));
+        uncapture(sout);
 
+        // Second ping.
+        const char* correct1 =
+                "60 bytes from 77:88:99:aa:bb:cc (18.52.86.120): "
+                "index=1 time=";
+        sout = capture(1);
         pingip_recv(NULL, &pkthdr, packet);
+        stop_capture(sout);
         fail_unless(numrecvd == prev_numrecvd + 2,
                     "numrecvd not incremented second time");
+        fail_unless(!strncmp(sout->buffer, correct1, strlen(correct1)));
+        uncapture(sout);
 } END_TEST
 
+/**
+ * Set up test suite.
+ */
 static Suite *
 arping_suite (void)
 {
@@ -65,6 +212,9 @@ arping_suite (void)
         return s;
 }
 
+/**
+ *
+ */
 int
 main()
 {
